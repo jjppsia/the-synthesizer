69345876a2c58bcecf99fc1266017c7e3cf504ea
Jp Sia <johnpaulsia4@gmail.com>
2023-05-06 16:58:31 +0800

    feat: add NoAudioMessage component

---
+++ src/components/no-audio-message/index.ts
@@ -0,0 +1,2 @@
+export * from './no-audio-message'
+export { default } from './no-audio-message'
---
+++ src/components/no-audio-message/no-audio-message.tsx
@@ -0,0 +1,9 @@
+function NoAudioMessage() {
+  return (
+    <div>
+      <p>It seems that your browser doesn't support audio API</p>
+    </div>
+  )
+}
+
+export default NoAudioMessage
